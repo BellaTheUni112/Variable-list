99907a114296285974617e462001b7130d1fc38a
BellaTheUni112 <bellatheunicorn521@gmail.com>
2025-08-13 23:22:00 +0200

    Update variablelist.bat

--- variablelist.bat
+++ variablelist.bat
@@ -6,13 +6,14 @@ echo ----------by Turkey----------
 echo.
 echo.
 echo $ = percent
-echo $time$ -current time
-echo $~dp0 -current path
-echo $~dd0 -current drive
-echo $~dt0 -current date and time
-echo $username$ -user running the script
+echo $time$ -current time. for example: the time is %time%
+echo $~dp0 -current path. for example: the path this script is running in is %~dp0
+echo $~dd0 -current drive. for example: the drive this script is running in is %~dd0
+echo $~dt0 -current date and time. for example: the current date and time is %~dt0
+echo $username$ -user running the script. for example: you are %username%
 echo Make sure to check @BellaTheUni112 on Github for any updates!
 pause >nul
 cls
 
 goto :start
+
