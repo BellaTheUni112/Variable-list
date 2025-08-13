2409fea1fbc86a75f1dbe9b31dcc181389dfd2eb
BellaTheUni112 <bellatheunicorn521@gmail.com>
2025-08-13 23:09:37 +0200

    Add files via upload

---
+++ variablelist.bat
@@ -0,0 +1,16 @@
+@echo off
+title Built-in variable list by Turkey
+:start
+echo --------Variable list--------
+echo ----------by Turkey----------
+echo.
+echo.
+echo $ = percent
+echo $time$ -current time
+echo $~dp0 -current path
+echo $~dd0 -current drive
+echo $~dt0 -current date and time
+echo Make sure to check @BellaTheUni112 on Github for any updates!
+pause >nul
+cls
+goto :start
