b4ce1136cb50e7ca0b3572884cf34d0c1fb9f29d
BellaTheUni112 <bellatheunicorn521@gmail.com>
2025-08-13 23:18:54 +0200

    Update variablelist.bat

--- variablelist.bat
+++ variablelist.bat
@@ -10,7 +10,9 @@ echo $time$ -current time
 echo $~dp0 -current path
 echo $~dd0 -current drive
 echo $~dt0 -current date and time
+echo $username$ -user running the script
 echo Make sure to check @BellaTheUni112 on Github for any updates!
 pause >nul
 cls
+
 goto :start
